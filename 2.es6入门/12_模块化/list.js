c3270f931fa0bd72824217f0e378579f6c177d3e
yuxiaoya111 <yuxiaoya@yicai.com>
2017-08-30 11:57:27 +0800

    update es6

---
+++ 2.es6入门/12_模块化/list.js
@@ -0,0 +1,5 @@
+/**
+ * Created by yxy123 on 2017/8/1.
+ */
+import app from 'module.js'
+console.log(app());
---
+++ 2.es6入门/12_模块化/module.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yxy123 on 2017/8/1.
+ */
+export function app(){
+    count=1;
+    return count;
+}
